2e69a60fcc5216c613c7dd849e4087f8681b3136
ecaleb97 <valley_rob.0c@icloud.com>
2024-01-17 18:01:06 +0100

    Add login and register functionality

---
+++ actions/register.ts
@@ -0,0 +1,37 @@
+'use server'
+
+import { z } from 'zod'
+import { RegisterSchema } from '@/schemas'
+import bcrypt from 'bcryptjs'
+import { db } from '@/lib/db'
+import { getUserByEmail } from '@/data/user'
+
+export async function register (values: z.infer<typeof RegisterSchema>) {
+  const validatedFields = RegisterSchema.safeParse(values)
+  if (!validatedFields.success) {
+    return {
+      error: "Invalid fields"
+    }
+  }
+
+  const { email, password, name } = validatedFields.data
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) {
+    return { error: "Email already exists" }
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    }
+  })
+
+  // TODO: Send verification token email
+
+  return { success: "Account created!" }
+}
