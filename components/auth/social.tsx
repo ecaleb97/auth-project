aa3754783228609159ae84024a63fee9f2729fa9
ecaleb97 <valley_rob.0c@icloud.com>
2024-01-29 14:59:28 +0100

    Add new components and update existing components

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,17 +1,25 @@
 'use client'
 
+import { signIn } from "next-auth/react"
 import { FcGoogle } from "react-icons/fc"
 import { FaGithub } from "react-icons/fa"
 import { Button } from "@/components/ui/button"
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes"
 
 export function Social () {
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT
+    })
+  }
+
   return (
     <div className="flex items-center w-full gap-x-2">
       <Button 
         variant="outline" 
         size="lg"
         className="w-full"
-        onClick={() => console.log("Google")}
+        onClick={() => onClick("google")}
       >
         <FcGoogle className="size-5" />
       </Button>
@@ -19,7 +27,7 @@ export function Social () {
         variant="outline" 
         size="lg"
         className="w-full"
-        onClick={() => console.log("Github")}
+        onClick={() => onClick("github")}
       >
         <FaGithub className="size-5" />
       </Button>
